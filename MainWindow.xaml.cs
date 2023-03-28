f4191d77ecf1e40361a9bcdff9c3f4a4fa4cf198
Tseng-Chih-Chung <chiao105031@gmail.com>
2023-03-28 15:23:51 +0800

    進度
    1.完成0~9數字鍵
    2.把前置的0在打第一個數字時及時清除

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,5 +29,95 @@ namespace computer
         {
             txt_number.Text = "0";
         }
+
+        private void btc1_Click(object sender, RoutedEventArgs e)
+        {
+            if(txt_number.Text == "0")
+            {
+                txt_number.Text = "";
+            }
+            txt_number.Text += "1";
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_number.Text == "0")
+            {
+                txt_number.Text = "";
+            }
+            txt_number.Text += "2";
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_number.Text == "0")
+            {
+                txt_number.Text = "";
+            }
+            txt_number.Text += "3";
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_number.Text == "0")
+            {
+                txt_number.Text = "";
+            }
+            txt_number.Text += "4";
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_number.Text == "0")
+            {
+                txt_number.Text = "";
+            }
+            txt_number.Text += "5";
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_number.Text == "0")
+            {
+                txt_number.Text = "";
+            }
+            txt_number.Text += "6";
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_number.Text == "0")
+            {
+                txt_number.Text = "";
+            }
+            txt_number.Text += "7";
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_number.Text == "0")
+            {
+                txt_number.Text = "";
+            }
+            txt_number.Text += "8";
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_number.Text == "0")
+            {
+                txt_number.Text = "";
+            }
+            txt_number.Text += "9";
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_number.Text == "0")
+            {
+                txt_number.Text = "";
+            }
+            txt_number.Text += "0";
+        }
     }
 }
