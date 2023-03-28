10ca29e048cd765bc6f5c302ce7cfef2d3dcf4fb
Tseng-Chih-Chung <chiao105031@gmail.com>
2023-03-28 22:58:02 +0800

    進度
    1.把所有按鍵的功能完成
    2.加入註解
    3.-會有bug(因為未清除原先的-導致負負得正)(稍後解決)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,9 +25,17 @@ namespace computer
             InitializeComponent();
         }
 
+        double Firstnumber;//第一個數值
+        double Secondnumber;//第二個數值
+        int operation;//+-*/的名稱
+
         private void btnAC_Click(object sender, RoutedEventArgs e)
         {
             txt_number.Text = "0";
+            Firstnumber = 0;
+            Secondnumber = 0;
+            operation = -1;
+            //只要按AllClean(AC)就把一切重製
         }
 
         private void btc1_Click(object sender, RoutedEventArgs e)
@@ -37,6 +45,7 @@ namespace computer
                 txt_number.Text = "";
             }
             txt_number.Text += "1";
+            //如果預設數只有0就清零再添加對應數字
         }
 
         private void btn2_Click(object sender, RoutedEventArgs e)
@@ -118,6 +127,71 @@ namespace computer
                 txt_number.Text = "";
             }
             txt_number.Text += "0";
+        }
+
+        private void btn_plus_Click(object sender, RoutedEventArgs e)
+        {
+            Firstnumber = Convert.ToSingle(txt_number.Text);
+            //ToSingle只是把字串轉成浮點數(用Todouble也行)
+            txt_number.Text = "+";
+            operation = 0;//輸出+的代號
+        }
+
+        private void btn_reduce_Click(object sender, RoutedEventArgs e)
+        {
+            Firstnumber = Convert.ToSingle(txt_number.Text);
+            txt_number.Text = "-";
+            operation = 1;//輸出-的代號
+        }
+
+        private void btn_multiplication_Click(object sender, RoutedEventArgs e)
+        {
+            Firstnumber = Convert.ToSingle(txt_number.Text);
+            txt_number.Text = "*";
+            operation = 2;//輸出*的代號
+        }
+
+        private void btn_division_Click(object sender, RoutedEventArgs e)
+        {
+            Firstnumber = Convert.ToSingle(txt_number.Text);
+            txt_number.Text = "/";
+            operation = 3;//輸出/的代號
+        }
+
+        private void btn_equal_Click(object sender, RoutedEventArgs e)
+        {
+            double Ans = 0;//建立名為Ans的浮點數
+            Secondnumber = Convert.ToSingle(txt_number.Text);
+            switch(operation)
+            {
+                case 0:
+                    Ans = Firstnumber + Secondnumber;
+                    break;
+                case 1:
+                    Ans = Firstnumber - Secondnumber;
+                    break;
+                case 2:
+                    Ans = Firstnumber * Secondnumber;
+                    break;
+                case 3:
+                    Ans = Firstnumber / Secondnumber;
+                    break;
+            }
+            txt_number.Text = string.Format("{0:0.#####}", Ans);
+            Firstnumber = 0;
+            Secondnumber = 0;
+            Ans = 0;
+            //做完運算後把一切浮點數歸零
+        }
+
+        private void btn_decimal_point_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_number.Text.IndexOf(".")==-1)
+                //Indexof是要去判斷有沒有規定的字串出現
+            {
+                txt_number.Text += ".";
+            }
+            //如果沒有 . 才會加 .
         }
     }
 }
